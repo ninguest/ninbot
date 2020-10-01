6684f0c62ffc530e4202518660a7653d31d8690f
NineGuest <teeyucheng123@gmail.com>
2020-10-01 14:37:05 +0800

    Update News Command
    / edit news.js and delete fixed search in news command

--- resources/utils.js
+++ resources/utils.js
@@ -162,3 +162,12 @@ exports.WarningOnUnvalidIndexInput = (sizeorlengthnum, message)=>{
   else return message.reply(`You've entered invalid index number. The range of valid index number is \`1\` to \`${sizeorlengthnum}\``);
 }
 
+exports.isURL = (str) =>{
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
